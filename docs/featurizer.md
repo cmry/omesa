c9659eb24654e8bec5266ac17c3bf185b19c6e0d
fazzeh <c.emmery@outlook.com>
2015-12-18 19:19:24 -0100

    added doc2md, test with featurizer docs

--- docs/featurizer.md
+++ docs/featurizer.md
@@ -1,1 +1,383 @@
-# Featurizer
+# Text feature extraction module
+
+
+This module contains several helper classes for extracting textual features
+used in Text Mining applications, partly based on instances parsed with parse.
+It also includes a wrapper class to cleverly hanlde this within the shed
+environment.
+
+
+
+ ## Featurizer 
+
+``` python 
+ class Featurizer(features) 
+```
+
+Wrapper for looping feature extractors in fit and transform operations.
+
+Calls helper classes which extract different features from text data. Given
+a list of initialized feature extractor classes, correctly streams or dumps
+instances along these classes. Also provides an interface to fit and
+transform methods.
+
+| Parameters    | Type             | Doc             |
+|:-------|:-----------------|:----------------|
+        | features | list | List of initialized feature extractor classes. The classes can befound within this module. |
+        
+
+| Attributes    | Type             | Doc             |
+|:-------|:-----------------|:----------------|
+        | helper | list of classes |         Store for the provided features. |
+        | Y | list of labels | Store for the provided features.Labels for X. |
+        
+
+------- 
+
+##Examples
+
+Note: this is just for local use only.
+
+During training with a full space and a generator:
+
+``` python 
+ 
+>>> loader = reader.load  # assumes that this is a generator
+>>> features = [Ngrams(level='char', n_list=[1,2])]
+>>> ftr = _Featurizer(features)
+>>> ftr.fit(loader())
+>>> X, Y = ftr.transform(loader()), ftr.labels
+ 
+```
+
+
+During testing with only one instance:
+
+
+``` python 
+ 
+>>> new_data = 'this is some string to test'
+>>> tex, tey = ftr.transform(new_data), ftr.labels
+ 
+```
+
+
+
+--------- 
+
+## Methods 
+
+ 
+
+| Function    | Doc             |
+|:-------|:----------------|
+        | transform | Call all the helpers to extract features. |
+         
+ 
+
+### transform
+
+``` python 
+    transform(stream) 
+```
+
+
+Call all the helpers to extract features.
+
+| Parameters    | Type             | Doc             |
+|:-------|:-----------------|:----------------|
+        | stream | generator |             Yields an instance with (label, raw, parse, meta). |
+        
+
+| Returns    | Type             | Doc             |
+|:-------|:-----------------|:----------------|
+        | X | numpy array of shape [n_samples, n_features] | Training data returns when applying the transform function. |
+        
+
+
+ ## Ngrams 
+
+``` python 
+ class Ngrams(object) 
+```
+
+Calculate n-gram frequencies.
+
+Can either be applied on token, POS or character level. The transform
+method dumps a feature dictionary that can be used for feature hashing.
+
+| Parameters    | Type             | Doc             |
+|:-------|:-----------------|:----------------|
+        | n_list | list of integers |         Amount of grams that have to be extracted, can be multiple. Say that        uni and bigrams have to be extracted, n_list has to be [1, 2]. |
+        
+
+------- 
+
+##Examples
+
+Token-level uni and bigrams with a maximum of 2000 feats per n:
+
+
+``` python 
+ 
+>>> ng = Ngrams(level='token', n_list=[1, 2], max_feats=2000)
+>>> ng.transform('this is text')
+... {'this': 1, 'is': 1, 'text': 1, 'this is': 1, 'is text': 1}
+ 
+```
+
+
+
+--------- 
+
+## Methods 
+
+ 
+
+| Function    | Doc             |
+|:-------|:----------------|
+        | __str__ | Report on feature settings. |
+        | _find_ngrams | Magic n-gram function. |
+        | transform | Given a document, return level-grams as Counter dict. |
+         
+ 
+
+### __str__
+
+``` python 
+    __str__() 
+```
+
+
+Report on feature settings.
+
+### _find_ngrams
+
+``` python 
+    _find_ngrams(input_list, n) 
+```
+
+
+Magic n-gram function.
+
+Calculate n-grams from a list of tokens/characters with added begin and
+end items. Based on the implementation by Scott Triglia.
+
+### transform
+
+``` python 
+    transform(raw, parse=None) 
+```
+
+
+Given a document, return level-grams as Counter dict.
+
+
+ ## FuncWords 
+
+``` python 
+ class FuncWords(object) 
+```
+
+Extract function word frequencies.
+
+Computes relative frequencies of function words according to parse data,
+and adds the respective frequencies as a feature.
+
+--------- 
+
+## Methods 
+
+ 
+
+| Function    | Doc             |
+|:-------|:----------------|
+        | transform | Extract frequencies for fitted function word possibilites. |
+         
+ 
+
+### transform
+
+``` python 
+    transform(_, parse) 
+```
+
+
+Extract frequencies for fitted function word possibilites.
+
+
+ ## SentimentFeatures 
+
+``` python 
+ class SentimentFeatures() 
+```
+
+Lexicon based sentiment features.
+
+Calculates four features related to sentiment: average polarity, number of
+positive, negative and neutral words. Counts based on the Duoman and
+Pattern sentiment lexicons.
+
+--------- 
+
+## Methods 
+
+ 
+
+| Function    | Doc             |
+|:-------|:----------------|
+        | __str__ | Calculate four features for the input instance. |
+        | transform | Get the sentiment belonging to the words in the parse string. |
+         
+ 
+
+### __str__
+
+``` python 
+    __str__() 
+```
+
+
+Calculate four features for the input instance.
+
+Instance is a list of word-pos-lemma tuples that represent a token.
+
+### transform
+
+``` python 
+    transform(_, parse) 
+```
+
+
+Get the sentiment belonging to the words in the parse string.
+
+
+ ## SimpleStats 
+
+``` python 
+ class SimpleStats 
+```
+
+
+
+| Parameters    | Type             | Doc             |
+|:-------|:-----------------|:----------------|
+        | text | boolean, optional, default True |  |
+        | token | boolean, optional, default True |  |
+        | sentence_lenth | boolean, optional, default True | Add the sentence length as a feature. |
+        
+
+------- 
+
+##Examples
+
+All features:
+
+``` python 
+ 
+>>> SimpleStats()
+ 
+```
+
+
+Only text features:
+
+
+``` python 
+ 
+>>> SimpleStats(token=False, sentence_length=False)
+ 
+```
+
+
+
+--------- 
+
+## Methods 
+
+ 
+
+| Function    | Doc             |
+|:-------|:----------------|
+        | avg | Average length of iter. |
+        | text_based_feats | Include features that are based on the raw text. |
+        | token_based_feats | Include features that are based on certain tokens. |
+        | avg_sent_length | Calculate average sentence length. |
+        | transform | Transform given instance into simple text features. |
+         
+ 
+
+### avg
+
+``` python 
+    avg(iterb) 
+```
+
+
+Average length of iter.
+
+### text_based_feats
+
+``` python 
+    text_based_feats(raw) 
+```
+
+
+Include features that are based on the raw text.
+
+### token_based_feats
+
+``` python 
+    token_based_feats(tokens) 
+```
+
+
+Include features that are based on certain tokens.
+
+### avg_sent_length
+
+``` python 
+    avg_sent_length(sentence_indices) 
+```
+
+
+Calculate average sentence length.
+
+### transform
+
+``` python 
+    transform(raw, parse) 
+```
+
+
+Transform given instance into simple text features.
+
+
+ ## Readability 
+
+``` python 
+ class Readability 
+```
+
+Get readability-related features.
+
+--------- 
+
+## Methods 
+
+ 
+
+| Function    | Doc             |
+|:-------|:----------------|
+        | transform | Add each metric to the feature vector. |
+         
+ 
+
+### transform
+
+``` python 
+    transform(raw, _) 
+```
+
+
+Add each metric to the feature vector.
---
+++ shed/doc2md.py
@@ -0,0 +1,221 @@
+"""Converts sklearn-styled docstrings to markdown."""
+
+from sys import argv
+
+# Author:       Chris Emmery
+# License:      MIT
+# pylint:       disable=W0142
+
+ERR = '''
+- """ used for non-docstring strings.
+- Class or function Documentation has newline after """.
+'''
+
+
+class Doc2Markdown:
+
+    """Documentation to Markdown."""
+
+    def __init__(self, file_in, file_out):
+        """Handle in/out to readers and parsers."""
+        self.file_in = file_in
+        self.file_out = file_out
+        self.markdown = ''
+
+        self.md_table = '''
+
+| {0}    | Type             | Doc             |
+|:-------|:-----------------|:----------------|
+        '''
+        self.md_table_row = '''| {0} | {1} | {2} |
+        '''
+
+        self.md_table_alt = '''
+
+| {0}    | Doc             |
+|:-------|:----------------|
+        '''
+        self.md_table_row_alt = '''| {0} | {1} |
+        '''
+
+        self.md_code = '``` python \n {0} \n```\n\n'
+
+        self.read()
+
+    def read(self):
+        """Open pytyhon code file, spit out markdown file."""
+        with open(self.file_in, 'r') as file_in:
+            file_open = file_in.read()
+            classes = file_open.split('\nclass ')
+            module_doc = classes.pop(0)  # top of the file docstring
+            self.markdown = [self.md_title(module_doc)]
+            self.handle_classes(classes)
+        with open(self.file_out, 'w') as file_out:
+            file_out.write('\n'.join(self.markdown))
+
+    def handle_classes(self, classes):
+        """Given class docs only, write out their docs and functions."""
+        for clss in classes:
+            clss = " class " + clss  # got cut-off in split
+            clss_str = clss.split('"""\n')[:-1]
+            # process all the class information
+            self.markdown.append(
+                self.md_class_doc(self.split_doc(clss_str.pop(0))))
+            # replace object part with __init__ params
+            self.markdown[1] = \
+                self.md_class_func(self.markdown[1], clss_str)
+            # start handling def
+            self.markdown.append(
+                self.md_funcs_doc([self.split_doc(func) for func in clss_str]))
+
+    @staticmethod
+    def split_doc(doc):
+        """Split a python file on docstring."""
+        lines = [x for x in doc.split('\n') if x]
+        name, det = '', False
+        # skip lines until class/def, end if :
+        while ':' not in name:
+            try:
+                if 'class' in lines[0] or 'def' in lines[0]:
+                    det = True
+            except IndexError:
+                exit("Malformed code. \n\nSome common errors: {0}".format(ERR))
+            line = lines.pop(0)
+            if det:
+                name += line
+
+        parts = {'name': name}
+        buffer_to = 'title'
+        # if ----- is found, set title to above, else buffer to name
+        for i in range(0, len(lines)):
+            line = lines[i]
+            if '---' in line:
+                buffer_to = lines[i-1]
+                continue
+            if not parts.get(buffer_to):
+                parts[buffer_to] = [buffer_to]
+            if parts.get(buffer_to) and line:
+                parts[buffer_to].append(line)
+        # if line is shorter than 3, likely that there is no previous section
+        return {k.replace('  ', ''):
+                (v[:-1] if len(v) > 2 else v)[1:] for k, v in parts.items()}
+
+    @staticmethod
+    def md_title(title_part, class_line=False):
+        """Handle title and text parts for main and class-specific."""
+        buff, start, second = [], False, False
+        # pretty convoluted function just to append # to first line and clean
+        for line in title_part.split('\n'):
+            line = line.replace('  ', '')
+            if line.startswith('"""') and not start:
+                title = '# ' + line.replace('"""', '')[:-1]
+                if class_line:
+                    title = title[2:] + '.'
+                buff.append(title)
+                start = True
+            elif line.startswith('"""') and start:
+                start = False
+                return '\n'.join(buff)
+            elif start:
+                if not second and not line.startswith('\n'):
+                    line = "\n" + line
+                    second = True
+                buff.append(line)
+        return '\n'.join(buff)
+
+    def md_par_att(self, doc, name):
+        """Place parameters and attributes in a table overview."""
+        head, buff, lines = '', (), []
+        table = self.md_table.format(name)
+        # given var : type \n description, splits these up into 3 cellss
+        if doc:
+            for row in doc:
+                if ':' in row and not buff:
+                    head = row.split(' : ')
+                    for part in head:
+                        buff += (part.replace('  ', ''), )
+                elif ':' in row and buff:
+                    buff += (''.join(lines), )
+                    table += self.md_table_row.format(*buff)
+                    buff = ()
+                elif buff:
+                    lines.append(row.replace('\n', ' '))
+            if buff:
+                buff += (''.join(lines).replace('  ', ''), )
+                table += self.md_table_row.format(*buff)
+            return table
+        else:
+            return ''
+
+    def md_examples(self, doc):
+        """Section text and code in the example part of the doc."""
+        head = '\n\n------- \n\n##Examples\n\n{0}'
+        order = []
+        text, code = '', ''
+        # if we find some code related beginnings (>>>, ...) buffer to code,
+        # else we regard it as text. Record order so that multiple blocks of
+        # text and code are possible.
+        if doc:
+            for row in doc:
+                if '>>>' in row or '...' in row:
+                    if text:
+                        order.append(text)
+                        text = ''
+                    row = row.replace('    >>>', '>>>')
+                    row = row.replace('    ...', '...')
+                    code += row + '\n'
+                else:
+                    if code:
+                        order.append(self.md_code.format('\n' + code))
+                        code = ''
+                    row = row.replace('  ', '')
+                    text += row + '\n'
+            if text or code:
+                order.append(text)
+                order.append(self.md_code.format('\n' + code))
+            return head.format('\n'.join(order).replace('.\n', '.\n\n'))
+        else:
+            return ''
+
+    def md_class_func(self, doc, clss):
+        """Replace class ...(object) with __init__ arguments."""
+        init_doc = self.split_doc(clss.pop(0))['name']
+        init_doc = init_doc.replace('  ', '')
+        init_doc = init_doc.replace(' def __init__(self, ', '(')
+        return doc.replace('(object)', init_doc)
+
+    def md_funcs_doc(self, func_doc):
+        """Merge all function elements in one string."""
+        mark_doc, mark_head = '', '\n--------- \n\n## Methods \n\n {0} \n {1}'
+        func_table = self.md_table_alt.format('Function')
+        for func in func_doc:
+            name = func['name'].replace('(', ' (').split()[1]
+            title = self.md_title('\n'.join(func['title']), class_line=True)
+            func_table += self.md_table_row_alt.format(name,
+                                                       title.split('\n')[0])
+            mark_doc += '\n\n### ' + name + '\n\n'
+
+            cod = self.md_code.format(func['name'].replace('def ', ''))
+            cod = cod.replace('self', '').replace('(, ', '(')
+
+            mark_doc += cod
+            mark_doc += '\n' + title
+            mark_doc += self.md_par_att(func.get('Parameters'), 'Parameters')
+            mark_doc += self.md_par_att(func.get('Returns'), 'Returns')
+        return mark_head.format(func_table, mark_doc)
+
+    def md_class_doc(self, class_doc):
+        """Merge all class section elements in one string."""
+        mark_doc = ''
+        mark_doc += '\n\n ## {0} \n\n'.format(
+            class_doc['name'].replace('(', ' (').split()[1])
+        mark_doc += self.md_code.format(class_doc['name'])
+        mark_doc += self.md_title('\n'.join(class_doc['title']),
+                                  class_line=True)
+        mark_doc += self.md_par_att(class_doc.get('Parameters'), 'Parameters')
+        mark_doc += self.md_par_att(class_doc.get('Attributes'), 'Attributes')
+        mark_doc += self.md_examples(class_doc.get('Examples'))
+        return mark_doc
+
+if __name__ == '__main__':
+    D2M = Doc2Markdown(argv[1], argv[2])
