f3f68756e0a21599f9a65cf30b176ed7975846b6
fazzeh <c.emmery@outlook.com>
2016-06-14 17:44:31 +0200

    try abspath fix15

--- examples/deployment.py
+++ examples/deployment.py
@@ -1,6 +1,6 @@
 from omesa.containers import Pipeline
 
-pl = Pipeline(name='20_news', store='db')
+pl = Pipeline(name='20_news_grams', store='db')
 pl.load()
 
 print(pl.classify(["some raw text"]))
