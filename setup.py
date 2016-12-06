35c5327b9c101b8a7401ee83c4a661ea83a2e64b
cmry <c.emmery@outlook.com>
2016-12-06 18:17:59 +0100

    fixed annoy dep

--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ from setuptools import find_packages
 
 
 setup(name='omesa',
-      version='0.3.2.alpha',
+      version='0.4.0.alpha',
       description='A framework for reproducible machine learning research',
       author='Chris Emmery',
-      author_email='chris.emmery@uantwerpen.be',
+      author_email='cmry@protonmail.com',
       url='https://github.com/cmry/omesa',
       license='GPLv3',
       packages=find_packages(exclude=['tests']),
-      install_requires=['blitzdb>=0.2.12',
+      install_requires=['annoy>=1.8.0',
+                        'blitzdb>=0.2.12',
                         'bottle>=0.12.9',
                         'colorlover>=0.2.1',
                         'lime>=0.1.1.7',
